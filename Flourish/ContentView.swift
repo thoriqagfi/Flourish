9f7e4b84347ef8c39b8dd33de3e6449d2e57e979
Agfi <thoriq.aghfi60@gmail.com>
2024-06-23 15:08:01 +0700

    Initial Commit

---
+++ Flourish/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flourish
+//
+//  Created by Agfi on 23/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flourish/FlourishApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlourishApp.swift
+//  Flourish
+//
+//  Created by Agfi on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlourishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
